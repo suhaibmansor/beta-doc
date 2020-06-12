1b9547b4448872b7f0ae4015448dc1a4a95ecc04
bizobjek_apple <bizobjek_apple@bizobjek-apples-MacBook-Pro.local>
2020-06-12 20:42:24 +0800

    base files added

---
+++ src/layouts/Default.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container mx-auto px-4">
+    <header class="header">
+      <strong>
+        <g-link to="/">{{ $static.metadata.siteName }}</g-link>
+      </strong>
+      <nav class="nav">
+        <g-link class="nav__link" to="/">Home</g-link>
+        <g-link class="nav__link" to="/about/">About</g-link>
+      </nav>
+    </header>
+    <slot/>
+  </div>
+</template>
+
+<static-query>
+query {
+  metadata {
+    siteName
+  }
+}
+</static-query>
+
+<style>
+
+</style>
